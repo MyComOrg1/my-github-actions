1a788a840151b00c579a249ae087a4eed1016723
yen <yeunnah@hotmail.com>
2022-09-15 14:20:44 +0800

    fix app registration output

--- infra/modules/terraform-azuread-application/output.tf
+++ infra/modules/terraform-azuread-application/output.tf
@@ -1,9 +1,9 @@
 output "application_id" {
-  value = data.azuread_application.ad-app.application_id
+  value = azuread_application.ad-app.application_id
 }
 output "display_name" {
-  value = data.azuread_application.ad-app.display_name
+  value = azuread_application.ad-app.display_name
 }
 output "object_id" {
-  value = data.azuread_application.ad-app.object_id
+  value = azuread_application.ad-app.object_id
 }
