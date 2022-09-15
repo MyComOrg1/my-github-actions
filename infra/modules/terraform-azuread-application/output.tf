7239fd0d6d14bee1249e8c4847221bc27f196db1
yen <yeunnah@hotmail.com>
2022-09-15 13:59:52 +0800

    add app registration output

--- infra/modules/terraform-azuread-application/output.tf
+++ infra/modules/terraform-azuread-application/output.tf
@@ -1,9 +1,9 @@
 output "application_id" {
-  value = azuread_application.application_id
+  value = azuread_application.ad-app.application_id
 }
 output "display_name" {
-  value = azuread_application.display_name
+  value = azuread_application.ad-app.display_name
 }
 output "object_id" {
-  value = azuread_application.object_id
+  value = azuread_application.ad-app.object_id
 }
